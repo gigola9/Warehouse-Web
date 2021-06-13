2a7c0de4fa9ceab9228deb4b1ed9a8d31a872bcc
Giorgi <gigola9@yahoo.com>
2021-06-13 22:34:55 +0400

    Commit unstaged files

---
+++ src/app/shared/models/ActivitiesModel.ts
@@ -0,0 +1,12 @@
+export interface ActivitiesModel {
+  id: number;
+  username: string;
+  type?: string;
+  amount?: string;
+  date?: Date | string;
+  productCode?: number;
+  comment?: string;
+  giver?: string;
+  productId?: number;
+  productName?: number;
+}
